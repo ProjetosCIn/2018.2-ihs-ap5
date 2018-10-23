81b81cd53a5d6d01e152d7bc0f207cd7a8b54847
luizkof <38214081+luizkof@users.noreply.github.com>
2018-10-23 14:32:53 -0300

    modificado

--- arquivo.c
+++ arquivo.c
@@ -1,24 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-extern void cone(float*,float,float);
+extern void calculaSeno(double,double,int*);//(valor de x em rad, valor da diferença e qtd de iteraçoes)
+//a função retorna o valor calculado para sin x
 
 
 #define PI 3.14159265359
 int main()
 {
-    double numero;
+    double angulo;
     printf("Digite um angulo: ");
-    scanf(" %lf", &numero);
+    scanf(" %lf", &angulo);
 
 
-    numero = numero*(2*PI/360);
+    angulo = angulo*(2*PI/360);
 
     double E;
     printf("Digite a diferença máxima entre o seno calculado e o seno real");
     scanf(" %lf", &E);
 
-    printf("\nSeu angulo em radiano é : %0.11lf", numero);
+    int* numeroIteracao;
+    //calculaSeno(angulo,E,numeroIteracao);
+
+    printf("\nSeu angulo em radiano é : %0.11lf", angulo);
     printf("\nA diferença máxima é : %0.11lf\n", E);
     return 0;
 }
